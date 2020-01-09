e6c6e44342e96881bc190777d23b5358a6fd6d76
Michele Mastrogiovanni <michele.mastrogiovanni@konicaminolta.it>
2020-01-09 11:29:16 +0100

    Upgrade version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='host-manager',  
-    version='0.1',
+    version='0.1.1',
     scripts=['host-manager'] ,
     author="Michele Mastrogiovanni",
     author_email="michele.mastrogiovanni@gmail.com",
